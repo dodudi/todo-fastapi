3a284be3c1f1a7443f15c9a11d6477172a60819c
Rudy <idejrdud@gmail.com>
2026-03-09 10:28:01 +0900

    feat: Todo 수정 테스트 코드 추가
    - #6

--- tests/test_todos.py
+++ tests/test_todos.py
@@ -26,11 +26,20 @@ def client(session):
     app.dependency_overrides.clear()
 
 
+todo = {
+    "title": "test todo",
+    "description": "test description",
+    "priority": "low",
+}
+
+update_todo = {
+    "title": "updated title",
+    "status": True,
+}
+
+
 def test_create_todo(client):
-    response = client.post("/todos", json={
-        "title": "test todo",
-        "priority": "low",
-    })
+    response = client.post("/todos", json=todo)
 
     assert response.status_code == 200
     data = response.json()
@@ -39,11 +48,7 @@ def test_create_todo(client):
 
 
 def test_read_todo(client):
-    create = client.post("/todos", json={
-        "title": "test todo",
-        "description": "test description",
-        "priority": "low",
-    })
+    create = client.post("/todos", json=todo)
     todo_id = create.json()["id"]
 
     response = client.get(f"/todos/{todo_id}")
@@ -54,3 +59,14 @@ def test_read_todo(client):
     assert data["title"] == "test todo"
     assert data["description"] == "test description"
     assert data["priority"] == "low"
+
+
+def test_update_todo(client):
+    create = client.post("/todos", json=todo)
+    todo_id = create.json()["id"]
+
+    response = client.patch(f"/todos/{todo_id}", json=update_todo)
+    data = response.json()
+    assert response.status_code == 200
+    assert data["title"] == "updated title"
+    assert data["status"] is True
